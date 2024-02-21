9c2ac14974441c08160b1c9bd6ea55e305c44ade
bvvreddy <vamshi8667@gmail.com>
2024-02-21 03:40:06 -0500

    Added asserts to inputs

--- practice.py
+++ practice.py
@@ -1,8 +1,10 @@
 class Person:
     def __init__(self, name=str, age=int):
+        assert isinstance(name, str)
+        assert isinstance(age, int)
         self.name = name
         self.age = age
     
-person = Person('Vamshi', 27)
+person = Person("Vamshi", 27)
 print(Person(__name__))
         
